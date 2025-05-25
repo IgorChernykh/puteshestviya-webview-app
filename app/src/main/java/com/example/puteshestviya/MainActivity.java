7ebad9b9406a9f7c357e653960869478ab2c650d
IgorChernykh <igor@coffeestudio.ru>
2025-05-25 11:34:30 +0300

    init: webview project

---
+++ app/src/main/java/com/example/puteshestviya/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.puteshestviya;
+
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.app.Activity;
+
+public class MainActivity extends Activity {
+    private WebView webView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        webView = findViewById(R.id.webview);
+        webView.setWebViewClient(new WebViewClient());
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        webView.loadUrl("https://pskovatmosfera.ru/");
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (webView.canGoBack()) {
+            webView.goBack();
+        } else {
+            super.onBackPressed();
+        }
+    }
+}
